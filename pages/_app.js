0846b906e6e92abe6159a949d75e7174699cc29b
Yudawardana94 <yuda.wardana40@gmail.com>
2022-12-07 02:42:24 +0700

    add wizard 1 and 2 feature

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import { Provider } from "react-redux";
 import { useStore } from "../store";
 
 import "../styles/globals.css";
+// import "antd/dist/antd.css";
+import "antd/dist/antd";
 
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
--- pages/wizards/image.js
+++ pages/wizards/image.js
@@ -1,45 +1,168 @@
-import React, { useState } from "react";
+/* eslint-disable @next/next/no-img-element */
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import { Button, Upload, Row, Col, Slider, Modal } from "antd";
+import { DeleteTwoTone } from "@ant-design/icons";
 
 import styles from "../../styles/Wizards.module.css";
+
+const getBase64 = (img, callback) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result));
+  reader.readAsDataURL(img);
+};
+
 function Image() {
   const navigate = useRouter();
-  const [ktp, setKTP] = useState(null);
+  const { userData } = useSelector((state) => state);
+
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
+  const [userDataLocal, setUserDataLocal] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [zoomVal, setZoomVal] = useState(0);
 
   const providedData = ["No. Polisi", "Nama Tertanggung", "No. Polis"];
 
-  const onNavigateToScreen = (screen) => navigate.push(`/wizards/${screen}`);
+  useEffect(() => {
+    setUserDataLocal({
+      "No. Polisi": userData.licence,
+      "Nama Tertanggung": userData.name,
+      "No. Polis": userData.polisId,
+    });
+  }, []);
+
+  const onNavigateToScreen = (screen) => {
+    if (screen === "back") {
+      navigate.back();
+      return;
+    }
+    navigate.push(`/wizards/${screen}`);
+  };
   const onClaimButtonClicked = () => {
     // Condition to handle if photo is not uploaded yet
-    if (!ktp) return false;
+    if (!imageUrl) return false;
     onNavigateToScreen("summary");
   };
-  const onKtpUploaded = (e) => {
-    setKTP("Oke");
+
+  const handleChange = (info) => {
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === "done") {
+      getBase64(info.file.originFileObj, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
   };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const onSliderChange = (val) => setZoomVal(val);
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </div>
+  );
   return (
     <div className={styles.container}>
-      <div>
-        <p>{"<-"} Back Home</p>
+      <div className={styles.content}>
         <div>
-          <p>Unggah Foto KTP</p>
-        </div>
-        <div>
-          {providedData.map((req) => {
-            return <p key={req}>{req}</p>;
-          })}
-        </div>
-        <div>
-          <p>Foto KTP</p>
-          <div onClick={(e) => onKtpUploaded(e)}>
-            <p>Ini misalkan fotonya</p>
+          <button onClick={() => onNavigateToScreen("back")}>back</button>
+          <div className={styles.process}>
+            <p>Unggah Foto</p>
           </div>
-          <p>* Data pada KTP harus terlihat jelas.</p>
+          <div className={styles.inputGroup}>
+            {Object.entries(userDataLocal).map(([key, value]) => {
+              return (
+                <Row key={key} className={styles.gridRequired}>
+                  <Col span={10}>
+                    <b>{key}</b>
+                  </Col>
+                  <Col span={14}>
+                    <p>{value}</p>
+                  </Col>
+                </Row>
+              );
+            })}
+          </div>
+          <div>
+            <div className={styles.imageUploadWrapper}>
+              <h4 className={styles.titleText}>Foto KTP</h4>
+              {imageUrl ? (
+                <div onClick={() => setIsModalOpen(true)}>
+                  <img
+                    src={imageUrl}
+                    alt="avatar"
+                    className={styles.uploadedImage}
+                  />
+                </div>
+              ) : (
+                <Upload
+                  name="avatar"
+                  listType="picture-card"
+                  className="avatar-uploaders"
+                  showUploadList={false}
+                  onChange={handleChange}
+                >
+                  {uploadButton}
+                </Upload>
+              )}
+              <p className={styles.bottomNoteText}>
+                * Data pada KTP harus terlihat jelas.
+              </p>
+            </div>
+            <div className={styles.deletePhotoWrapper}>
+              <div
+                onClick={() => setImageUrl(null)}
+                className={styles.deletePhoto}
+              >
+                <DeleteTwoTone twoToneColor="#dc143c" />
+                <p className={styles.buttonText}>hapus foto</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div onClick={onClaimButtonClicked}>
-        <p>Ajukan Sekarang</p>
+      <div>
+        <Button
+          type="primary"
+          onClick={onClaimButtonClicked}
+          className={styles.bottomButton}
+        >
+          Berikutnya
+        </Button>
       </div>
+      {/* <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+      >
+        <div>
+          <img
+            src={imageUrl}
+            alt="avatar"
+            className={styles.uploadedImage}
+            style={{
+              transform: `scale(${100 + zoomVal}%)`,
+            }}
+          />
+          <Slider defaultValue={0} onChange={onSliderChange} />
+        </div>
+      </Modal> */}
     </div>
   );
 }
